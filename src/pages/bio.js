a164000bf56684f3ae777c1358a885d4a06334fa
birdswitharms <segal.a@hotmail.com>
2024-11-24 22:45:15 -0500

    Update bio

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -6,23 +6,24 @@ const Bio = () => {
     <div className="flex min-h-screen flex-col text-left px-6 sm:flex-row">
       <div className="p-5 sm:p-24 items-right text-lg text-white my-4 w-full  justify-center">
         <div className="mb-2">
-          Don Englert was born in Detroit, MI. where he studied the Accordian at
-          age 5, a few years later he moved to Los Angeles. In high school he
-          started studying the Saxophone and later studied the Clarinet, Flute,
-          Oboe, and the English Horn.
+          Don was born in Detroit Michigan where he began studying the Accordion
+          at age 5. A few years later his family moved to Los Angeles, where he
+          continued his musical studies. In high school he started studying the
+          Alto Saxophone and later added the Clarinet, Flute, Oboe, and English
+          Horn. At California State University Northridge, he majored in
+          Clarinet Performance. He attended the Navy School of Music in
+          Washington D.C. while in the Army.
         </div>
         <div>
-          He later studied music at California State University, Northridge and
-          the Navy School Of Music in Washington D.C. while in the Army. He
-          toured for nine years with bands such as Buddy Rich, Louis Bellson,
-          and the Canadian rock band Lighthouse. He is a multi woodwind
-          instrumentalist, and freelance musician. He has played in the pit
-          orchestra for more than 20 productions in Toronto and Stratford, ON,
-          recorded for film, radio and TV, and played on more than 40
-          recordings, including two Grammy nominations and three platinum
-          records.
+          After Military service he toured for 9 years with bands such as Buddy
+          Rich, Louis Bellson, and the Canadian Rock Band Lighthouse. He settled
+          in Toronto, Canada where he has worked as a multi- woodwind
+          instrumentalist and freelance musician. He has played in the pit
+          orchestra for more than 20 productions in Toronto and Stratford,
+          Ontario, and recorded for film, radio, and TV. He has played on more
+          than 40 recordings, including two Grammy nominations and three
+          platinum records.
         </div>
-
         <br />
       </div>
     </div>
